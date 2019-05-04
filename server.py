e9767c1e40d0ca540fc19b9f7e3d87ae1e584ce2
Polys Georgiou <polys@users.noreply.github.com>
2019-05-04 12:46:22 +0100

    Use `black` for code formatting

--- server.py
+++ server.py
@@ -8,17 +8,17 @@ from flask_httpauth import HTTPTokenAuth
 from jwcrypto import jwk, jws, jwt
 
 app = Flask(__name__)
-auth = HTTPTokenAuth(scheme='Bearer')
+auth = HTTPTokenAuth(scheme="Bearer")
 
-cache_opts = {'cache.type': 'memory'}
+cache_opts = {"cache.type": "memory"}
 cache = CacheManager(**parse_cache_config_options(cache_opts))
 
 
-@cache.cache('token_signing_keys', expire=300)
+@cache.cache("token_signing_keys", expire=300)
 def _get_token_signing_keys():
-    oid_config_url = 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration'
+    oid_config_url = "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration"
     oid_config = requests.get(oid_config_url).json()
-    jwks_uri = oid_config.get('jwks_uri')
+    jwks_uri = oid_config.get("jwks_uri")
     jwks = requests.get(jwks_uri).content
     keys = jwk.JWKSet.from_json(jwks)
     return keys
@@ -26,7 +26,7 @@ def _get_token_signing_keys():
 
 @auth.error_handler
 def _auth_error_handler():
-    return ''
+    return ""
 
 
 @auth.verify_token
@@ -46,7 +46,7 @@ def _auth_verify_token(id_token):
     return g.user_claims is not None
 
 
-@app.route('/me', methods=['GET'])
+@app.route("/me", methods=["GET"])
 @auth.login_required
 def index():
     return jsonify(g.user_claims)
@@ -57,5 +57,5 @@ def not_found(error=None):
     return Response(status=404)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
